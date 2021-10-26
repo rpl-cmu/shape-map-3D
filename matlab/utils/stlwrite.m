983b48b1bab433b876294f31abde3f2f70b7ef23
Sudharshan Suresh <suddhus@gmail.com>
2021-10-26 13:46:06 -0400

    Small change

--- matlab/utils/stlwrite.m
+++ matlab/utils/stlwrite.m
@@ -175,11 +175,8 @@ elseif isnumeric(varargin{1})
                 faces = delaunay(X,Y);
                 vertices = [X(:) Y(:) Z(:)];
             else
-                if ~exist('mesh2tri','file')
-                    error('stlwrite:missing', '"mesh2tri" is required to convert X,Y,Z matrices to STL. It can be downloaded from:\n%s\n',...
-                        'http://www.mathworks.com/matlabcentral/fileexchange/28327')
-                end
-                [faces, vertices] = mesh2tri(X, Y, Z, options.triangulation);
+                error('stlwrite:missing', '"mesh2tri" is required to convert X,Y,Z matrices to STL. It can be downloaded from:\n%s\n',...
+                    'http://www.mathworks.com/matlabcentral/fileexchange/28327')
             end
             
         case 2 % stlwrite('file', FACES, VERTICES, ...)
@@ -225,38 +222,6 @@ IP.parse(varargin{:});
 options = IP.Results;
 if ~isempty(options.facecolour)
     options.facecolor = options.facecolour;
-end
-
-function [F,V]=mesh2tri(X,Y,Z,tri_type)
-% function [F,V]=mesh2tri(X,Y,Z,tri_type)
-% 
-% Available from http://www.mathworks.com/matlabcentral/fileexchange/28327
-% Included here for convenience. Many thanks to Kevin Mattheus Moerman
-% kevinmoerman@hotmail.com
-% 15/07/2010
-%------------------------------------------------------------------------
-
-[J,I]=meshgrid(1:1:size(X,2)-1,1:1:size(X,1)-1);
-
-switch tri_type
-    case 'f'%Forward slash
-        TRI_I=[I(:),I(:)+1,I(:)+1;  I(:),I(:),I(:)+1];
-        TRI_J=[J(:),J(:)+1,J(:);   J(:),J(:)+1,J(:)+1];
-        F = sub2ind(size(X),TRI_I,TRI_J);
-    case 'b'%Back slash
-        TRI_I=[I(:),I(:)+1,I(:);  I(:)+1,I(:)+1,I(:)];
-        TRI_J=[J(:)+1,J(:),J(:);   J(:)+1,J(:),J(:)+1];
-        F = sub2ind(size(X),TRI_I,TRI_J);
-    case 'x'%Cross
-        TRI_I=[I(:)+1,I(:);  I(:)+1,I(:)+1;  I(:),I(:)+1;    I(:),I(:)];
-        TRI_J=[J(:),J(:);    J(:)+1,J(:);    J(:)+1,J(:)+1;  J(:),J(:)+1];
-        IND=((numel(X)+1):numel(X)+prod(size(X)-1))';
-        F = sub2ind(size(X),TRI_I,TRI_J);
-        F(:,3)=repmat(IND,[4,1]);
-        Fe_I=[I(:),I(:)+1,I(:)+1,I(:)]; Fe_J=[J(:),J(:),J(:)+1,J(:)+1];
-        Fe = sub2ind(size(X),Fe_I,Fe_J);
-        Xe=mean(X(Fe),2); Ye=mean(Y(Fe),2);  Ze=mean(Z(Fe),2);
-        X=[X(:);Xe(:)]; Y=[Y(:);Ye(:)]; Z=[Z(:);Ze(:)];
 end
 
 V=[X(:),Y(:),Z(:)];
